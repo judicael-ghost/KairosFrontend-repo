b9d182ce30ae693ffe6eb510ebe7eb1a8b7e251f
Judih rak <judihrak11@gmail.com>
2021-10-15 09:44:40 +0200

    package

--- serveur.js
+++ serveur.js
@@ -3,10 +3,10 @@ const path = require("path");
 
 const app = express();
 
-app.use(express.static(__dirname+'/dist/kairosFrontend'));
+app.use(express.static(__dirname+'/dist/KairosFrontend'));
 
 app.get('/*', function(req,res){
-  res.sendFile(path.join(__dirname+'/dist/kairosFrontend/index.html'));
+  res.sendFile(path.join(__dirname+'/dist/KairosFrontend/index.html'));
 })
 
 app.listen(process.env.PORT)
